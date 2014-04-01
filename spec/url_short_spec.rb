0bee83e5c6190c12699405bdd2a794839106640b
Paul Wenig <pwenig@gmail.com>
2014-04-01 14:55:07 -0600

    [Finish #68382564] User can input a url and see a shortened url-styled
    Signed-off-by: Keri Clowes <kerilyn.clowes@gmail.com>

--- spec/url_short_spec.rb
+++ spec/url_short_spec.rb
@@ -9,22 +9,17 @@ feature 'Creating URL Shortener' do
     visit("/")
     expect(page).to have_button("Shorten")
     expect(page).to have_field('url_to_be_shortened')
-  end
-
-  scenario "User fills in the shortener field" do
-    visit("/")
     fill_in("url_to_be_shortened", :with => 'http://google.com')
     click_on("Shorten")
-  end
-
-  scenario "sees both the original url and the shortened url" do
-    visit('/shorten')
     expect(page).to have_content('Original URL')
     expect(page).to have_content('"Shortened" URL')
     expect(page).to have_content('http://google.com')
     expect(page).to have_content('http://lit-river-4368.herokuapp.com/1') # Heroku staging URL
+    click_on('"Shorten" another URL')
+    expect(page).to have_field('url_to_be_shortened')
   end
 end
+
 
 
 
